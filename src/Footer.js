a00c1f7aeb69fcfd90fc6dfb51463af3bd5d177c
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-21 12:09:10 +0100

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <ul>
+      <li>About</li>
+      <li>Careers</li>
+      <li>Pricing</li>
+      <li>Feedback</li>
+      <li>Roadmappppp</li>
+    </ul>
+  )
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = ({ title }) => {
+  return (
+    <div className="header-container">
+      <nav className="topnav">
+        <ul>
+          <li href="#home">About</li>
+          <li href="#news">Trips</li>
+          <li href="#contact">Pricing</li>
+          <li href="#about">Contact</li>
+        </ul>
+      </nav>
+      <h1>Get ready for {title}!</h1>
+    </div>
+  );
+}
+
+export default Header;
